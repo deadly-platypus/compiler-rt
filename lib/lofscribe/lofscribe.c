ea4cfffb993c16c717158933d374209bed8e2d2d
Derrick McKee <derrick.mckee@gmail.com>
2018-09-03 16:12:05 -0400

    adding print outs

--- lib/lofscribe/lofscribe.c
+++ lib/lofscribe/lofscribe.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 
 void lof_precall(char numargs) {
-
+    printf("lof_precall called with numargs = %d\n", numargs);
 }
 
 void lof_record_arg(void* parameter, int isPointer) {
-
+    printf("lof_record_arg called with parameter = %08x and is%s a pointer\n", parameter, isPointer ? "" : " NOT");
 }
 
 void lof_postcall(void* returnValue, int isPointer) {
-
+    printf("lof_postcall called with returnValue = %08x and is%s a pointer\n", returnValue, isPointer ? "" : " NOT");
 }
