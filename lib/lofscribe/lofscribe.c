d23b29e0bfc7775c8ca15b06fc41f359324aa05d
Derrick McKee <derrick.mckee@gmail.com>
2018-09-04 14:05:11 -0400

    outputting data in JSON to a file

--- lib/lofscribe/lofscribe.c
+++ lib/lofscribe/lofscribe.c
@@ -3,15 +3,57 @@
 #include <string.h>
 #include "lofscribe.h"
 
-
 /* Current function being recorded. curr->data is a Stack* */
 static LLNode* curr;
+FILE* out;
+
+static int hasPrinted = 0;
+
+static void cleanup() {
+    if(out) {
+        fprintf(out, "]");
+        fclose(out);
+    }
+}
+
+static void output_arg(FuncArg* arg, int isLast) {
+    if(!arg) {
+        return;
+    }
+
+    switch(arg->typeId) {
+        case FloatTyID:
+            fprintf(out, "{ \"type\": %d, \"value\": %f }", arg->typeId, arg->value.fval);
+            break;
+        case DoubleTyID:
+            fprintf(out, "{ \"type\": %d, \"value\": %g }", arg->typeId, arg->value.dval);
+            break;
+        case IntegerTyID:
+            fprintf(out, "{ \"type\": %d, \"value\": %d }", arg->typeId, arg->value.ival);
+            break;
+        case PointerTyID:
+            fprintf(out, "{ \"type\": %d, \"value\": \"%p\" }", arg->typeId, arg->value.pval);
+            break;
+        default:
+            // TODO: Implement the rest
+            fprintf(out, "{ \"type\": %d, \"value\": \"%p\" }", arg->typeId, arg->value.pval);
+            break;
+    }
+
+    if(!isLast) {
+        fprintf(out, ", ");
+    }
+}
 
 static void free_function(LLNode* node) {
+    if(!node) {
+        return;
+    }
     Stack* s = (Stack*)node->data;
 
     LLNode* curr = stack_pop(s);
     while(curr) {
+        output_arg((FuncArg*)curr->data, s->size == 0);
         free(curr->data);
         free(curr);
         curr = stack_pop(s);
@@ -20,6 +62,10 @@ static void free_function(LLNode* node) {
     free(s->bottom);
     free(s);
     free(node);
+}
+
+static inline int isPointerLike(TypeID typeId) {
+    return (typeId == ArrayTyID || typeId == PointerTyID);
 }
 
 static FuncArg* create_arg(Data value, TypeID typeId) {
@@ -37,18 +83,25 @@ static FuncArg* create_arg(Data value, TypeID typeId) {
 void lof_precall(void* funcaddr) {
     if(functionCalls == NULL) {
         functionCalls = create_stack();
+        out = fopen("lof-output.json", "w+");
+        if(!out) {
+            fprintf(stderr, "Could not open lof-output.json! Exiting...\n");
+            exit(1);
+        }
+        atexit(cleanup);
     }
 
-    curr = NULL;
-    printf("lof_precall called function at %p\n", funcaddr);
+    Stack* s = create_stack();
+    s->bottom->data = funcaddr;
+    curr = create_node(s);
+    stack_push(functionCalls, curr);
+
+    //printf("lof_precall called function at %p\n", funcaddr);
 }
 
 void lof_record_arg(Data parameter, TypeID typeId) {
-    printf("lof_record_arg called with parameter = %p and is%s a pointer\n", 
-            parameter.pval, typeId == PointerTyID ? "" : " NOT");
-    if(!curr) {
-        curr = create_node(create_stack());
-    }
+    /*printf("lof_record_arg called with parameter = %p and is%s a pointer\n",
+            parameter.pval, isPointerLike(typeId) ? "" : " NOT");*/
 
     Stack *s = (Stack*)curr->data;
 
@@ -59,10 +112,30 @@ void lof_record_arg(Data parameter, TypeID typeId) {
 }
 
 void lof_postcall(Data returnValue, TypeID typeId) {
-    printf("lof_postcall called with returnValue = %p and is%s a pointer\n", 
-            returnValue.pval, typeId == PointerTyID ? "" : " NOT");
-    
+    /*printf("lof_postcall called with returnValue = %p and is%s a pointer\n",
+            returnValue.pval, isPointerLike(typeId) ? "" : " NOT");*/
+
+    FuncArg retVal;
+    retVal.typeId = typeId;
+    retVal.value = returnValue;
+
     LLNode* node = stack_pop(functionCalls);
+    Stack* s = (Stack*)node->data;
+    if(hasPrinted) {
+        fprintf(out, ",\n");
+    } else {
+        fprintf(out, "[");
+    }
+
+    fprintf(out,
+            "{ \"function\": {"
+            "\"addr\": \"%p\", "
+            "\"return\": ",
+            s->bottom->data);
+    output_arg(&retVal, 0);
+    fprintf(out, "\"args\": [");
 
     free_function(node);
+    fprintf(out, "] } }");
+    hasPrinted = 1;
 }
