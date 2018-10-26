f06919e7b284b370e30575c574f81aaf55c95415
Derrick McKee <derrick.mckee@gmail.com>
2018-10-26 17:22:39 -0400

    fixing access issues

--- lib/lofscribe/linkedlist.c
+++ lib/lofscribe/linkedlist.c
@@ -3,19 +3,19 @@
 #include <string.h>
 #include "lofscribe.h"
 
-void stack_push(Stack* stack, LLNode* value) {
+void stack_push(Stack *stack, LLNode *value) {
     stack->top->next = value;
     value->prev = stack->top;
     stack->top = value;
     stack->size++;
 }
 
-LLNode* stack_pop(Stack* stack) {
-    if(stack->top == stack->bottom) {
+LLNode *stack_pop(Stack *stack) {
+    if (stack->top == stack->bottom) {
         return NULL;
     }
 
-    LLNode* result = stack->top;
+    LLNode *result = stack->top;
     stack->top = result->prev;
     stack->top->next = NULL;
     result->prev = NULL;
@@ -23,9 +23,9 @@ LLNode* stack_pop(Stack* stack) {
     return result;
 }
 
-LLNode* create_node(void* data) {
-    LLNode* result = (LLNode*)malloc(sizeof(LLNode));
-    if(!result) {
+LLNode *create_node(void *data) {
+    LLNode *result = (LLNode *) malloc(sizeof(LLNode));
+    if (!result) {
         fprintf(stderr, "Could not allocate node! Exiting...");
         exit(1);
     }
@@ -34,9 +34,9 @@ LLNode* create_node(void* data) {
     return result;
 }
 
-Stack* create_stack() {
-    Stack* result = (Stack*)malloc(sizeof(Stack));
-    if(!result) {
+Stack *create_stack() {
+    Stack *result = (Stack *) malloc(sizeof(Stack));
+    if (!result) {
         fprintf(stderr, "Could not allocate stack! Exiting...");
         exit(1);
     }
@@ -46,8 +46,8 @@ Stack* create_stack() {
     return result;
 }
 
-void free_node(LLNode* node) {
-    if(node) {
+void free_node(LLNode *node) {
+    if (node) {
         free(node);
     }
 }
--- lib/lofscribe/lofscribe.c
+++ lib/lofscribe/lofscribe.c
@@ -6,43 +6,67 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include "lofscribe.h"
+#include <sys/mman.h>
+#include <errno.h>
+
+#define MAX_ADDR ((void*)0x7FFFFFFFFFFFFFFF)
+
+#define DBG_PRINT(fmt, ...) { if(in_debug) { printf(fmt, ##__VA_ARGS__); } }
 
 /* Current function being recorded. curr->data is a Stack* */
-static LLNode* curr;
-FILE* out;
+static LLNode *curr;
+static FILE *out;
 
+static char in_debug;
+static char output_json;
+
+static unsigned int stack_depth;
 static int hasPrinted = 0;
 
 static void cleanup() {
-    if(out) {
+    if (out) {
         fprintf(out, "]");
         fclose(out);
     }
 }
 
-static void output_hex(void* ptr, u_int64_t size) {
-    if(!ptr) {
+static int make_readable_region(void* ptr, size_t size) {
+    char *c = (char *) ((long)ptr & ~(getpagesize() - 1));
+    errno = 0;
+    if(mprotect(c, (char*)ptr - c + size, PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
+        DBG_PRINT("Could not set mprotect on %p (ptr = %p): %s\n", c, ptr, strerror(errno));
+        return 0;
+    }
+
+    return 1;
+}
+
+static void output_hex(void *ptr, size_t size) {
+    if (!ptr || ptr > MAX_ADDR) {
         return;
     }
-    char* c = (char*)ptr;
-    for(u_int64_t i = 0; i < size; i++) {
-        char curr = c[i];
+    if(!make_readable_region(ptr, size)) {
+        return;
+    }
+
+    for (u_int64_t i = 0; i < size; i++) {
+        char curr = ((char*)ptr)[i];
 /*        if(curr == '"') {
             fprintf(out, "\\\\%c", curr);
         } else if(curr >= '!' && curr <= '~') {
             fprintf(out, "%c", curr);
         } else {*/
-            fprintf(out, "\\x%02x", curr);
+        fprintf(out, "\\x%02x", curr);
 //        }
     }
 }
 
-static void output_ptrval(FuncArg* arg) {
-    PtrVal* ptrval = (PtrVal*)arg->value.pval;
+static void output_ptrval(FuncArg *arg) {
+    PtrVal *ptrval = (PtrVal *) arg->value.pval;
     fprintf(out,
             "{ \"type\": %d, \"size\": %lu, \"precall\": \"",
             arg->typeId, arg->size);
-    if(ptrval == NULL) {
+    if (ptrval == NULL) {
         fprintf(out, "0");
     } else {
         output_hex(ptrval->value, arg->size);
@@ -56,12 +80,12 @@ static void output_ptrval(FuncArg* arg) {
             "\" }");
 }
 
-static void output_arg(FuncArg* arg, int isLast) {
-    if(!arg) {
+static void output_arg(FuncArg *arg, int isLast) {
+    if (!arg) {
         return;
     }
 
-    switch(arg->typeId) {
+    switch (arg->typeId) {
         case FloatTyID:
             fprintf(out, "{ \"type\": %d, \"value\": %f, \"size\": %lu }", arg->typeId, arg->value.fval, arg->size);
             break;
@@ -80,25 +104,26 @@ static void output_arg(FuncArg* arg, int isLast) {
             break;
     }
 
-    if(!isLast) {
+    if (!isLast) {
         fprintf(out, ", ");
     }
 }
 
-static void free_function(LLNode* node) {
-    if(!node) {
+static void free_function(LLNode *node) {
+    if (!node) {
         return;
     }
-    Stack* s = (Stack*)node->data;
+    Stack *s = (Stack *) node->data;
 
-    LLNode* curr = stack_pop(s);
-    while(curr) {
-        output_arg((FuncArg*)curr->data, s->size == 0);
+    LLNode *curr = stack_pop(s);
+    while (curr) {
+        output_arg((FuncArg *) curr->data, s->size == 0);
         free(curr->data);
         free(curr);
         curr = stack_pop(s);
     }
 
+    free(s->bottom->data);
     free(s->bottom);
     free(s);
     free(node);
@@ -108,39 +133,42 @@ static inline int isPointerLike(TypeID typeId) {
     return (typeId == ArrayTyID || typeId == PointerTyID);
 }
 
-static PtrVal* create_pointer_val(Data value, u_int64_t size) {
-    PtrVal* result = (PtrVal*)malloc(sizeof(PtrVal));
-    if(!result) {
+static PtrVal *create_pointer_val(Data value, size_t size) {
+    PtrVal *result = (PtrVal *) malloc(sizeof(PtrVal));
+    if (!result) {
         fprintf(stderr, "Could not allocate PtrVal! Exiting...\n");
         exit(1);
     }
-    result->value = (char*)malloc(size);
-    if(!result->value) {
+
+    result->value = (char *) malloc(size);
+    if (!result->value) {
         fprintf(stderr, "Could not allocate PtrVal.value! Exiting\n");
         exit(1);
     }
     result->loc = value.pval;
-    if(value.pval != NULL) {
-        memcpy(result->value, value.pval, size);
+    if (value.pval > NULL && value.pval < MAX_ADDR) {
+        if(make_readable_region(value.pval, size)) {
+            memcpy(result->value, value.pval, size);
+        }
     }
     return result;
 }
 
-static FuncArg* create_arg(Data value, TypeID typeId, u_int64_t size) {
-    FuncArg* arg = (FuncArg*)malloc(sizeof(FuncArg));
-    if(!arg) {
+static FuncArg *create_arg(Data value, TypeID typeId, size_t size) {
+    FuncArg *arg = (FuncArg *) malloc(sizeof(FuncArg));
+    if (!arg) {
         fprintf(stderr, "Could not allocate FuncArg! Exiting...\n");
         exit(1);
     }
-    
+
     arg->size = size;
     arg->typeId = typeId;
-    
+
     /* Read pointer data */
-    if(isPointerLike(typeId)) {
-        if(size == sizeof(char)) {
+    if (isPointerLike(typeId)) {
+        if (size == sizeof(char)) {
             /* This is a char* -- +1 to size for the null terminator */
-            if(value.pval == NULL) {
+            if (value.pval == NULL) {
                 arg->size = 0;
                 arg->value.pval = NULL;
             } else {
@@ -157,26 +185,41 @@ static FuncArg* create_arg(Data value, TypeID typeId, u_int64_t size) {
     return arg;
 }
 
-void lof_precall(char* funcname) {
-    if(functionCalls == NULL) {
+void lof_precall(char *funcname) {
+    if (functionCalls == NULL) {
+        if(getenv("LOF_DEBUG") && strcmp("true", getenv("LOF_DEBUG")) == 0) {
+            in_debug = 1;
+        }
+
         functionCalls = create_stack();
         char name[128];
         int counter = 0;
-        sprintf(name, "lof-output-%lu-%d.json", time(NULL), counter);
-        while(access(name, F_OK) != -1) {
-            sprintf(name, "lof-output-%lu-%d.json", time(NULL), ++counter);
+        if(getenv("LOF_OUTPUT_JSON") && strcmp(getenv("LOF_OUTPUT_JSON"), "false") == 0) {
+            strcpy(name, "/dev/null");
+        } else {
+            sprintf(name, "lof-output-%lu-%d.json", time(NULL), counter);
+            output_json = 1;
         }
 
+        if(output_json) {
+            while (access(name, F_OK) != -1) {
+                sprintf(name, "lof-output-%lu-%d.json", time(NULL), ++counter);
+            }
+        }
+
+
         out = fopen(name, "w+");
-        if(!out) {
+        if (!out) {
             fprintf(stderr, "Could not open lof-output.json! Exiting...\n");
             exit(1);
         }
         atexit(cleanup);
     }
 
-    Stack* s = create_stack();
-    s->bottom->data = funcname;
+    DBG_PRINT("Calling function %s (stack depth: %d)\n", funcname, stack_depth++);
+
+    Stack *s = create_stack();
+    s->bottom->data = strdup(funcname);
     curr = create_node(s);
     stack_push(functionCalls, curr);
 }
@@ -188,10 +231,10 @@ void lof_double_record_arg(TypeID typeId, size_t size, double parameter) {
 }
 
 void lof_record_arg(TypeID typeId, size_t size, Data parameter) {
-    Stack *s = (Stack*)curr->data;
+    Stack *s = (Stack *) curr->data;
 
     FuncArg *arg = create_arg(parameter, typeId, size / 8);
-    LLNode* node = create_node(arg);
+    LLNode *node = create_node(arg);
 
     stack_push(s, node);
 }
@@ -203,15 +246,20 @@ void lof_double_postcall(TypeID typeId, size_t size, double parameter) {
 }
 
 void lof_postcall(TypeID typeId, size_t size, Data returnValue) {
-    LLNode* node = stack_pop(functionCalls);
-    if(!node) {
+    LLNode *node = stack_pop(functionCalls);
+    if (!node) {
         return;
     }
 
-    FuncArg* retVal = create_arg(returnValue, typeId, size);
+    FuncArg *retVal = create_arg(returnValue, typeId, size);
 
-    Stack* s = (Stack*)node->data;
-    if(hasPrinted) {
+    Stack *s = (Stack *) node->data;
+    if(in_debug) {
+        DBG_PRINT("Exiting %s\n", s->bottom->data);
+        stack_depth--;
+    }
+
+    if (hasPrinted) {
         fprintf(out, ",\n");
     } else {
         fprintf(out, "{\"name\": \"%s\", \"functions\": [", getenv("_"));
@@ -227,7 +275,7 @@ void lof_postcall(TypeID typeId, size_t size, Data returnValue) {
 
     free_function(node);
     fprintf(out, "] } }");
-    if(isPointerLike(typeId)) {
+    if (isPointerLike(typeId)) {
         free(retVal->value.pval);
     }
     free(retVal);
